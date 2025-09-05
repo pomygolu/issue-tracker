a5876f448be9da737e77004057c1c7a2b35c7bcb
Rohit Dalmia <meetrohit1996@gmail.com>
2025-09-05 20:03:28 +0530

    Added dropdown and refactored navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,45 +5,68 @@ import { AiFillBug } from "react-icons/ai";
 import { usePathname } from 'next/navigation'
 import classnames from 'classnames'
 import {useSession} from 'next-auth/react'
-import { Box, Flex } from '@radix-ui/themes';
+import { Avatar, Box, DropdownMenu, Flex, Text } from '@radix-ui/themes';
+
 
 const NavBar = () => {
-    let links = [
-        { href: '/', label: 'Dashboard' },
-        { href: '/issues', label: 'Issues' },
-    ]
-    const currentPath = usePathname()
-    const {status, data: session} = useSession()
   return (
       <nav className='px-5 mb-5 border-b py-3'>
         <Flex justify='between'> 
             <Flex align='center' gap='4'>
             <Link href='/'><AiFillBug /></Link>
-                <ul className='flex items-center space-x-6'>
-                    {links.map((link) => (
-                        <li key={link.href}>
-                        <Link
-                            
-                            href={link.href}
-                            className={classnames({
-                                'text-gray-500': currentPath !== link.href,
-                                'text-gray-900': currentPath === link.href,
-                                'hover:text-black transition-colors': true,
-                            })}
-                            >
-                            {link.label}
-                        </Link>
-                        </li>
-                    ))}
-                </ul>
+                <NavLinks />
             </Flex>
-            <Box>
-            {status === "authenticated" && <Link href="/api/auth/signout">Sign Out</Link>}
-            {status === "unauthenticated" && <Link href="/api/auth/signin">Sign In</Link>}
-          </Box>
+            <AuthStatus />
         </Flex>
       </nav>
   );
+}
+
+const NavLinks = () => {
+    let links = [
+        { href: '/', label: 'Dashboard' },
+        { href: '/issues', label: 'Issues' },
+    ]
+    const currentPath = usePathname()
+    return (
+        <ul className='flex items-center space-x-6'>
+            {links.map((link) => (
+                <li key={link.href}>
+                <Link
+                    
+                    href={link.href}
+                    className={classnames({
+                        'nav-link': true,
+                        '!text-gray-900': currentPath === link.href
+                    })}
+                    >
+                    {link.label}
+                </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const AuthStatus = () => {
+    const {status, data: session} = useSession()
+    return (
+        <Box>
+        {status === "authenticated" && 
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                <Avatar src={session?.user?.image!} fallback='?' size='2' radius='full' className='cursor-pointer' referrerPolicy='no-referrer' />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+                <DropdownMenu.Label><Text size='2'>{session?.user?.email}</Text></DropdownMenu.Label>
+                <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Sign Out</Link>
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>}
+        {status === "unauthenticated" && <Link className='nav-link' href="/api/auth/signin">Sign In</Link>}
+      </Box>
+    )
 }
 
 export default NavBar
