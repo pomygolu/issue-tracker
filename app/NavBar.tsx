536c890c5ac63c77d0d91d28a234c36acbf088d2
Rohit Dalmia <meetrohit1996@gmail.com>
2025-09-05 19:24:12 +0530

    Styling navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { AiFillBug } from "react-icons/ai";
 import { usePathname } from 'next/navigation'
 import classnames from 'classnames'
 import {useSession} from 'next-auth/react'
-import { Box } from '@radix-ui/themes';
+import { Box, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
     let links = [
@@ -15,29 +15,33 @@ const NavBar = () => {
     const currentPath = usePathname()
     const {status, data: session} = useSession()
   return (
-      <nav className='flex items-center p-4 mx-5 space-x-6 h-15 border-b'>
-          <Link href='/'><AiFillBug /></Link>
-          <ul className='flex items-center space-x-6'>
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                      
-                      href={link.href}
-                      className={classnames({
-                          'text-gray-500': currentPath !== link.href,
-                          'text-gray-900': currentPath === link.href,
-                          'hover:text-black transition-colors': true,
-                      })}
-                      >
-                      {link.label}
-                  </Link>
-                  </li>
-              ))}
-          </ul>
-          <Box>
+      <nav className='px-5 mb-5 border-b py-3'>
+        <Flex justify='between'> 
+            <Flex align='center' gap='4'>
+            <Link href='/'><AiFillBug /></Link>
+                <ul className='flex items-center space-x-6'>
+                    {links.map((link) => (
+                        <li key={link.href}>
+                        <Link
+                            
+                            href={link.href}
+                            className={classnames({
+                                'text-gray-500': currentPath !== link.href,
+                                'text-gray-900': currentPath === link.href,
+                                'hover:text-black transition-colors': true,
+                            })}
+                            >
+                            {link.label}
+                        </Link>
+                        </li>
+                    ))}
+                </ul>
+            </Flex>
+            <Box>
             {status === "authenticated" && <Link href="/api/auth/signout">Sign Out</Link>}
             {status === "unauthenticated" && <Link href="/api/auth/signin">Sign In</Link>}
           </Box>
+        </Flex>
       </nav>
   );
 }
